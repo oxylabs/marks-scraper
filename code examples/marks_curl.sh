a6c3170ae50a20bbe7978c6e0295b2fdf40d0e11
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:45:34 +0200

    Adding code examples/marks_curl.sh

---
+++ code examples/marks_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.marksandspencer.com/l/women/all-new-in#intid=ww_dlp_logo-nav_08022024_txt_1_new_in"}'
