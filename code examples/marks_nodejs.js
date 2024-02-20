080234198144b087c1ee005fde742e082cf87f13
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:45:32 +0200

    Adding code examples/marks_nodejs.js

---
+++ code examples/marks_nodejs.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+const body = {
+  'source': 'universal_ecommerce',
+  'url': 'https://www.marksandspencer.com/l/women/all-new-in#intid=ww_dlp_logo-nav_08022024_txt_1_new_in',
+};
+const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+  }
+});
+
+console.log(await response.json());
