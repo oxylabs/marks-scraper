f692a50bcc576235e6fd2ee565a1f2ab02c9b153
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:45:36 +0200

    Adding code examples/marks_golang.go

---
+++ code examples/marks_golang.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	const Username = "YOUR_USERNAME"
+	const Password = "YOUR_PASSWORD"
+
+	payload := map[string]string{
+		"source": "universal_ecommerce",
+		"url": "https://www.marksandspencer.com/l/women/all-new-in#intid=ww_dlp_logo-nav_08022024_txt_1_new_in",
+	}
+
+	jsonValue, _ := json.Marshal(payload)
+
+	client := &http.Client{}
+	request, _ := http.NewRequest("POST",
+		"https://realtime.oxylabs.io/v1/queries",
+		bytes.NewBuffer(jsonValue),
+	)
+
+	request.SetBasicAuth(Username, Password)
+	response, _ := client.Do(request)
+
+	responseText, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(responseText))
+}
